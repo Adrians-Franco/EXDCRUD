e7e101a38efea1029113764b4251aae34e54c8ce
adrians franco <adriansfranco@gmail.com>
2025-11-12 21:28:17 -0300

    revisao back-end-bcrypt

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,84 +1,74 @@
-import ServiceUser from "../service/users.js"
+import ServiceUser from '../service/users.js'
 
 class ControllerUser {
-
-    FindAll(_, res) {
+    
+    async FindAll(_, res) {
         try {
-
-            const nome = ServiceUser.FindAll()
-            res.status(200).send({ nome })
-
+            const users = await ServiceUser.FindAll()
+            res.status(200).send({ users })
         } catch (error) {
             res.status(500).send({ error: error.message })
         }
-
     }
-
+    
     async FindOne(req, res) {
         try {
-            console.log(req.headers.user)
-            const id = req.params.id
+            const id = req.params.id || req.headers?.user?.id
 
             const user = await ServiceUser.FindOne(id)
             res.status(200).send({ user })
-
         } catch (error) {
             res.status(500).send({ error: error.message })
         }
-
     }
 
     async Create(req, res) {
         try {
-
+            const loggedUser = req.headers?.user
+            let permissao = 1
+            if(loggedUser && loggedUser.permissao === 0){
+                permissao = req.body.permissao
+            }
             const { nome, email, senha, ativo } = req.body
-            await ServiceUser.Create(nome, email, senha, ativo, 1)
-            res.status(201).send("Usuario criado com sucesso")
-
+            await ServiceUser.Create(nome, email, senha, ativo, permissao)
+            res.status(201).send()
         } catch (error) {
             res.status(500).send({ error: error.message })
         }
-
     }
-    async Update(req, res) {
+    
+    Update(req, res) {
         try {
-            const id = req.params.id
-            const { nome, email, senha, ativo } = req.body
-            ServiceUser.Update(id, nome, email, senha, ativo)
+            const id = req.params.id || req.headers?.user?.id
+            const nome = req.body.nome
+            ServiceUser.Update(id, nome)
             res.status(200).send()
-
         } catch (error) {
             res.status(500).send({ error: error.message })
         }
-
     }
-    async Delete(req, res) {
+    
+    Delete(req, res) {
         try {
-            const id = req.params.id
-            await ServiceUser.Delete(id)
-            res.status(204).send("usuario deletado com sucesso")
-
+            const id = req.params.id || req.headers?.user?.id
+            ServiceUser.Delete(id)
+            res.status(204).send()
         } catch (error) {
             res.status(500).send({ error: error.message })
         }
-
     }
 
     async Login(req, res) {
         try {
             const { email, senha } = req.body
-           // console.log(email,senha)
+
             const token = await ServiceUser.Login(email, senha)
+
             res.status(200).send({ token })
         } catch (error) {
             res.status(500).send({ error: error.message })
-
         }
-
-
-
     }
 
 }
-
 export default new ControllerUser()
--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,38 +1,85 @@
 import jwt from 'jsonwebtoken'
 import ServiceUser from '../service/users.js'
-
-const JWT_SEGREDO = "Guears"
-
-export default async function authMiddleware(req, res, next) {
-
-    try {
-
-        const token = req.headers['authorization']
+const JWT_SEGREDO ="Guears"
 
 
+export default function authMiddleware(roles = []) {
+    return async (req, res, next) => {
+        try {
 
-        if (!token) {
-            throw new Error("nao podix acessarix")
+            const token = req.headers['authorization']
+
+
+
+            if (!token) {
+                throw new Error("nao podix acessarix")
+            }
+
+            const decoded = jwt.verify(token.split(' ')[1], JWT_SEGREDO)
+            //token.split(' ')[1]
+
+            const user = await ServiceUser.FindOne(decoded.id)
+
+            if(roles.length && !roles.includes(user.permissao)) {
+                throw new Error("Voce não tem permissão para realizar esta ação")
+            }
+            req.headers.user = user
+
+
+            //console.log(decoded)
+
+            next()
+
+
+        } catch (error) {
+
+            res.status(403).send({
+                data: null,
+                msg: error.message,
+                error: true
+            })
+
         }
-
-        const decoded = jwt.verify(token.split(' ')[1], JWT_SEGREDO)
-        //token.split(' ')[1]
-
-        const user = await ServiceUser.FindOne(decoded.id)
-        req.headers.user = user
-
-        //console.log(decoded)
-
-        next()
-
-
-    } catch (error) {
-
-        res.status(403).send({
-            data: null,
-            msg: error.message,
-            error: true
-        })
-
     }
+
 }
+
+
+
+
+
+// const JWT_SEGREDO = "Guears"
+
+// export default async function authMiddleware(req, res, next) {
+
+//     try {
+
+//         const token = req.headers['authorization']
+
+
+
+//         if (!token) {
+//             throw new Error("nao podix acessarix")
+//         }
+
+//         const decoded = jwt.verify(token.split(' ')[1], JWT_SEGREDO)
+//         //token.split(' ')[1]
+
+//         const user = await ServiceUser.FindOne(decoded.id)
+//         req.headers.user = user
+
+//         //console.log(decoded)
+
+//         next()
+
+
+//     } catch (error) {
+
+//         res.status(403).send({
+//             data: null,
+//             msg: error.message,
+//             error: true
+//         })
+
+//     }
+// 
